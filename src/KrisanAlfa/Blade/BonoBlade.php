642b9689ed47251f54e1e504aac2f7fd05ce179f
Krisan Alfa Timur <krisan47@gmail.com>
2014-05-21 22:52:59 +0700

    [UPDATE] Strong typing and some coding standard

--- src/KrisanAlfa/Blade/BonoBlade.php
+++ src/KrisanAlfa/Blade/BonoBlade.php
@@ -1,39 +1,4 @@
-<?php
-
-/**
- * Bono - PHP5 Web Framework
- *
- * MIT LICENSE
- *
- * Copyright (c) 2013 PT Sagara Xinix Solusitama
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
- * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
- * OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
- * WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- *
- * @category  View
- * @package   Bono
- * @author    Krisan Alfa Timur <krisan47@gmail.com>
- * @copyright 2013 PT Sagara Xinix Solusitama
- * @license   https://raw.github.com/xinix-technology/bono/master/LICENSE MIT
- * @link      https://github.com/krisanalfa/bonoblade
- */
-namespace KrisanAlfa\Blade;
+<?php namespace KrisanAlfa\Blade;
 
 use Bono\App;
 use Illuminate\Container\Container;
@@ -244,7 +209,7 @@ class BonoBlade extends View
      *
      * @return void
      */
-    protected function registerPhpEngine($resolver)
+    protected function registerPhpEngine(EngineResolver $resolver)
     {
         $resolver->register('php', function () {
             return new PhpEngine;
@@ -258,7 +223,7 @@ class BonoBlade extends View
      *
      * @return void
      */
-    protected function registerBladeEngine($resolver)
+    protected function registerBladeEngine(EngineResolver $resolver)
     {
         $mySelf    = $this;
         $container = $this->container;
@@ -324,7 +289,7 @@ class BonoBlade extends View
      *
      * @return void
      */
-    public function setLayout($layout, $data = array())
+    public function setLayout($layout, array $data = array())
     {
         $app              = App::getInstance();
         $layout           = $app->theme->resolve($layout);
@@ -375,7 +340,7 @@ class BonoBlade extends View
 
         try {
             $compiled->__toString();
-        } catch (\RuntimeException $e) {
+        } catch (RuntimeException $e) {
             $app->error($e);
         }
 
